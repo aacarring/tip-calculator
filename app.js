347cd29c3cf1fbacf0ce85e0fa9f12bf79100c81
Alexa Carrington <acarring@wellesley.edu>
2020-12-08 12:54:51 -0500

    Fixed calculateTip function in js

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const buttons = document.querySelectorAll('.choices button');
 const total = document.querySelector('.total');
 
 function calculateTip() {
+    total.textContent = "Total: ";
+
     let priceInputValue = parseFloat(priceInput.value);
     let tipPercentage = parseFloat(this.textContent) / 100;
     let totalCost = 0;
 
+    if (priceInputValue === 0 || priceInputValue === "" || isNaN(priceInputValue)) {
+        alert("Please enter a number in the 'Price' field.");
+        return "";
+    }
+
     totalCost = parseFloat(priceInputValue + (priceInputValue * tipPercentage));
-    total.textContent += '$' + totalCost.toString();
+    total.textContent += '$' + totalCost.toFixed(2).toString();
 }
 
 buttons.forEach(button => button.addEventListener('click', calculateTip));
